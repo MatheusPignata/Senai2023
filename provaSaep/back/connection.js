dd505c015e590b6284516188909cbfe2f0829541
MatheusPignata <pignatamatheus@gmail.com>
2023-08-29 08:42:15 -0300

    att

--- provaSaep/back/connection.js
+++
@@ -1,9 +0,0 @@
-const con = require('mysql2');
-
-const connection = con.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    database: process.env.DATABASE
-});
-
-module.exports = connection;
